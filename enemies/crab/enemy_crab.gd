ff24ef081709d15830a489d750f9d9391a54807f
Devin Alexander <devinmalexander@gmail.com>
2025-01-19 08:04:00 -0600

    Enemy Damage using Hitboxes and Hurtboxes

--- enemies/crab/enemy_crab.gd
+++ enemies/crab/enemy_crab.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
+var enemy_death_effect = preload("res://enemies/enemy_death_effect.tscn")
+
 @export var patrol_points: Node
 @export var speed: int = 1500
+@export var health_amount: int = 3
 @export var wait_time: int = 3
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -86,6 +89,21 @@ func enemy_animations():
 
 func _on_timer_timeout() -> void:
 	can_walk = true
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	print("Hurtbox area entered")
+	if area.get_parent().has_method("get_damage_amount"):
+		var node = area.get_parent() as Node
+		health_amount -= node.damage_amount
+		print("Health amount: ", health_amount)
+		
+		if health_amount <= 0:
+			var enemy_death_effect_instance = enemy_death_effect.instantiate() as Node2D
+			enemy_death_effect_instance.global_position = global_position
+			get_parent().add_child(enemy_death_effect_instance)
+			queue_free()
+
+
 
 #var enemy_death_effect = preload("res://enemies/enemy_death_effect.gd")
 #
