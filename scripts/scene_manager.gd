e8550c4a9f973d262da227d056f523b344f885d6
Devin Alexander <devinmalexander@gmail.com>
2025-01-22 15:43:55 -0600

    Game Shader VFX Loading Screen and Scene Transition

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -10,10 +10,8 @@ func transition_to_scene(level: String):
 	var scene_path: String = scenes.get(level)
 	
 	if scene_path != null:
-		await get_tree().create_timer(1.0).timeout
+		var scene_transition_scene_instance = scene_transition_screen.instantiate()
+		get_tree().get_root().add_child(scene_transition_scene_instance)
+		await get_tree().create_timer(5.0).timeout
 		get_tree().change_scene_to_file(scene_path)
-		#var scene_transition_scene_instance = scene_transition_screen.instantiate()
-		#get_tree().get_root().add_child(scene_transition_scene_instance)
-		#await get_tree().create_timer(5.0).timeout
-		#get_tree().change_scene_to_file(scene_path)
-		#scene_transition_scene_instance.queue_free()
+		scene_transition_scene_instance.queue_free()
